20fd4a4a2bdcf74269da67b9ef71d9342a16bd87
sokolov <sokolov.ispp01@gmail.com>
2023-11-17 21:24:26 +0300

    add GroupController & UserController upd
    Добавлен контроллер для групп. В контроллере User в GET методе добавлена опциональная фильтрация по группам.

--- Web/Controllers/GroupController.cs
+++ Web/Controllers/GroupController.cs
@@ -1,4 +1,4 @@
-using Data.Context;
+п»їusing Data.Context;
 using Data.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -7,14 +7,14 @@ using System.Net;
 namespace Web.Controllers
 {
     [ApiController]
-    [Route("[controller]")]    
-    public class UserController : ControllerBase
+    [Route("[controller]")]
+    public class GroupController : ControllerBase
     {
-        private readonly ILogger<UserController> _logger;
+        private readonly ILogger<GroupController> _logger;
 
         private readonly ZerdaContext _dbContext;
 
-        public UserController(ILogger<UserController> logger, ZerdaContext dbContext)
+        public GroupController(ILogger<GroupController> logger, ZerdaContext dbContext)
         {
             _logger = logger;
             _dbContext = dbContext;
@@ -23,22 +23,15 @@ namespace Web.Controllers
         /// <summary>
         /// Get request
         /// </summary>
-        /// <param name="groupId">group id</param>
         /// <param name="limit">count records to get (max 50)</param>
         /// <param name="offset">starting position relative to the beginning of the table</param>
         /// <returns>List of objects</returns>
         /// <response code="200">Success</response>
-        [ProducesResponseType(typeof(IEnumerable<User>), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(IEnumerable<Group>), (int)HttpStatusCode.OK)]
         [HttpGet()]
-        public async Task<IActionResult> Get(
-            int? groupId = null,
-            int limit = 50, 
-            int offset = 0)
+        public async Task<IActionResult> Get(int limit = 50, int offset = 0)
         {
-            return StatusCode(200, await _dbContext.User
-                .Include(x => x.Account)
-                .Include(x => x.Group)
-                .Where(x => groupId == null || x.GroupId == groupId)
+            return StatusCode(200, await _dbContext.Group
                 .AsNoTracking()
                 .OrderBy(x => x.Id)
                 .Skip(offset)
@@ -53,13 +46,13 @@ namespace Web.Controllers
         /// <response code="201">Success adding</response>
         /// <response code="204">Duplicate object (state unchanged)</response>
         /// <returns>Created object</returns>
-        [ProducesResponseType(typeof(User), (int)HttpStatusCode.Created)]
+        [ProducesResponseType(typeof(Group), (int)HttpStatusCode.Created)]
         [HttpPost()]
-        public async Task<IActionResult> Post([FromBody] User obj)
+        public async Task<IActionResult> Post([FromBody] Group obj)
         {
             try
             {
-                await _dbContext.User.AddAsync(obj);
+                await _dbContext.Group.AddAsync(obj);
                 await _dbContext.SaveChangesAsync();
                 return StatusCode(201, obj);
             }
@@ -67,7 +60,7 @@ namespace Web.Controllers
             {
                 if (ex.InnerException is MySqlConnector.MySqlException && ex.InnerException.Message.Contains("Duplicate entry"))
                 {
-                    _logger.LogWarning("Попытка добавления дубликата");
+                    _logger.LogWarning("РџРѕРїС‹С‚РєР° РґРѕР±Р°РІР»РµРЅРёСЏ РґСѓР±Р»РёРєР°С‚Р°");
                     return StatusCode(204);
                 }
                 return StatusCode(500, "DbException");
@@ -92,7 +85,7 @@ namespace Web.Controllers
         {
             try
             {
-                User? obj = _dbContext.User.FirstOrDefault(x => x.Id == id);
+                Group? obj = _dbContext.Group.FirstOrDefault(x => x.Id == id);
                 if (obj is null)
                 {
                     return NotFound();
@@ -109,7 +102,7 @@ namespace Web.Controllers
                 if (ex.InnerException is MySqlConnector.MySqlException
                     && ex.InnerException.Message.Contains("Cannot delete or update a parent row"))
                 {
-                    _logger.LogWarning("Попытка удаления связанной записи");
+                    _logger.LogWarning("РџРѕРїС‹С‚РєР° СѓРґР°Р»РµРЅРёСЏ СЃРІСЏР·Р°РЅРЅРѕР№ Р·Р°РїРёСЃРё");
                     return StatusCode(409);
                 }
                 return StatusCode(500, "DbException");
