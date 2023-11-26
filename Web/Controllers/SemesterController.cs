b8e27b9295c336bfada169261a06366aad6a503a
sokolov <sokolov.ispp01@gmail.com>
2023-11-26 16:48:11 +0300

    semester controller add

---
+++ Web/Controllers/SemesterController.cs
@@ -0,0 +1,167 @@
+using Asp.Versioning;
+using Data.Context;
+using Data.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Net;
+
+namespace Web.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SemesterController : ControllerBase
+    {
+        private readonly ILogger<SemesterController> _logger;
+
+        private readonly ZerdaContext _dbContext;
+
+        public SemesterController(ILogger<SemesterController> logger, ZerdaContext dbContext)
+        {
+            _logger = logger;
+            _dbContext = dbContext;
+        }
+
+        #region GET
+        /// <summary>
+        /// Метод на семестров
+        /// </summary>
+        /// <param name="limit">количество записей (до 50)</param>
+        /// <param name="offset">смещение относительно начала таблицы</param>
+        /// <returns>список объектов</returns>
+        /// <response code="200">Успех</response>
+        [ProducesResponseType(typeof(IEnumerable<Semester>), (int)HttpStatusCode.OK)]
+        [HttpGet()]
+        public async Task<IActionResult> Get(int limit = 50, int offset = 0)
+        {
+            return StatusCode(200, await _dbContext.Semester
+                .AsNoTracking()
+                .OrderBy(x => x.Id)
+                .Skip(offset)
+                .Take(Math.Min(limit, 50))
+                .ToListAsync());
+        }
+        #endregion
+
+        #region POST
+        /// <summary>
+        /// Добавление семестра
+        /// </summary>
+        /// <response code="200">Не возвращается для этого метода</response>
+        /// <response code="201">Успешное добавление</response>
+        /// <response code="204">Попытка добавления дубликата (status quo)</response>
+        /// <returns>Созданный объект</returns>
+        [ProducesResponseType(typeof(Semester), (int)HttpStatusCode.Created)]
+        [HttpPost()]
+        public async Task<IActionResult> Post([FromBody] Semester obj)
+        {
+            try
+            {
+                await _dbContext.Semester.AddAsync(obj);
+                await _dbContext.SaveChangesAsync();
+                return StatusCode(201, obj);
+            }
+            catch (DbUpdateException ex)
+            {
+                if (ex.InnerException is MySqlConnector.MySqlException && ex.InnerException.Message.Contains("Duplicate entry"))
+                {
+                    _logger.LogWarning("Попытка добавления дубликата");
+                    return StatusCode(204);
+                }
+                return StatusCode(500, "DbException");
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+        }
+        #endregion
+
+        #region PUT
+        /// <summary>
+        /// Обновление семестра
+        /// </summary>
+        /// <response code="200">Не возвращается для этого метода</response>
+        /// <response code="201">Успешное обновление</response>
+        /// <returns>обновленный объект</returns>
+        [ProducesResponseType(typeof(Semester), (int)HttpStatusCode.Created)]
+        [HttpPut()]
+        public async Task<IActionResult> Put([FromBody] Semester obj)
+        {
+            try
+            {
+                Semester? Semester = await _dbContext.Semester
+                    .AsNoTracking()
+                    .FirstOrDefaultAsync(d => d.Id == obj.Id);
+                if (Semester is not null)
+                {
+                    _dbContext.Entry(obj).State = EntityState.Modified;
+                }
+                else
+                {
+                    _dbContext.Entry(obj).State = EntityState.Added;
+                }
+
+                await _dbContext.SaveChangesAsync();
+                return StatusCode(201, obj);
+            }
+            catch (DbUpdateException ex)
+            {
+                if (ex.InnerException is MySqlConnector.MySqlException && ex.InnerException.Message.Contains("Duplicate entry"))
+                {
+                    _logger.LogWarning("Попытка добавления дубликата");
+                    return StatusCode(204);
+                }
+                return StatusCode(500, "DbException");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+        #endregion
+
+        #region DELETE
+        /// <summary>
+        /// Удаление семестра
+        /// </summary>
+        /// <param name="id">идентификатор объекта</param>
+        /// <returns>HTTP ответ</returns>
+        /// <response code="200">Успешное удаление</response>
+        /// <response code="404">Объект для удаления не найден (status quo)</response>
+        /// <response code="409">Существует некаскадная связь (status quo)</response>
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                Semester? obj = _dbContext.Semester.FirstOrDefault(x => x.Id == id);
+                if (obj is null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    _dbContext.Entry(obj).State = EntityState.Deleted;
+                    await _dbContext.SaveChangesAsync();
+                    return StatusCode(200);
+                }
+            }
+            catch (DbUpdateException ex)
+            {
+                if (ex.InnerException is MySqlConnector.MySqlException
+                    && ex.InnerException.Message.Contains("Cannot delete or update a parent row"))
+                {
+                    _logger.LogWarning("Попытка удаления связанной записи");
+                    return StatusCode(409);
+                }
+                return StatusCode(500, "DbException");
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+
+        }
+        #endregion
+    }
+}
