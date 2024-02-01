71c6e90da67ef1eea7a0d14e8965250f9f7a002d
sokolov <sokolov.ispp01@gmail.com>
2024-02-01 12:31:04 +0300

    export pattern2 develop

--- Web/Controllers/ExportController.cs
+++ Web/Controllers/ExportController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Text;
 using System.Threading.Tasks;
+using Web.Model;
 
 namespace Web.Controllers
 {
@@ -28,7 +29,7 @@ namespace Web.Controllers
         }
 
         #region GET
-        [HttpGet("{groupId}/{disciplineId}")]
+        [HttpGet("{groupId}/{disciplineId}/{semesterId}")]
         public async Task<IActionResult> Get(
             int groupId,
             int disciplineId,
@@ -86,6 +87,77 @@ namespace Web.Controllers
             }
 
             return Ok(builder.ToString());
+        }
+
+
+        [HttpGet("pattern2/{groupId}/{disciplineId}")]
+        public async Task<IActionResult> GetPattern2(
+            int groupId,
+            int disciplineId,
+            int? semesterId = null)
+        {
+            if (semesterId is null)
+            {
+                semesterId = (int)_dbContext.Semester.ToList().MaxBy(x => x.StartYear + (x.IsSecond ? 0.5 : 0)).Id;
+            }
+
+
+            List<Result> results = _dbContext.Result
+                .AsNoTracking()
+                .Include(x => x.Student)
+                .Include(x => x.Work)
+                .Where(x => x.Student.GroupId == groupId && x.Work.SemesterId == semesterId && x.Work.DisciplineId == disciplineId)
+                .ToList();
+
+            Group selectedGroup = _dbContext.Group
+                .AsNoTracking()
+                .Include(x => x.Student)
+                .Include(x => x.Assignment)
+                    .ThenInclude(x => x.Work)
+                    .ThenInclude(x => x.WorkType)
+                .First(g => g.Id == groupId);
+
+            List<Assignment> assignedWorks = selectedGroup.Assignment
+                .Where(x => x.Work.DisciplineId == disciplineId && x.Work.SemesterId == semesterId)
+                .OrderBy(x => x.Work.WorkType!.Name)
+                    .ThenBy(x => x.Work.Number)
+                .ToList();
+
+
+
+            StringBuilder builder = new StringBuilder();
+            string[,] matrix = new string[20, 20];
+
+            List<Student> orderedStudents = selectedGroup.Student
+                .OrderBy(x => x.Surname)
+                    .ThenBy(x => x.Name)
+                    .ThenBy(x => x.Patronym)
+                    .ToList();
+
+
+            for (int i = 0; i < assignedWorks.Count; i++)
+            {
+                matrix[0, i + 3] = assignedWorks[i].Work.Number.ToString();
+            }
+
+            const int studentBoxOffset = 2;
+            for (int i = 0; i < selectedGroup.Student.Count; i++)
+            {
+                matrix[i + studentBoxOffset, 0] = (i + 1).ToString();
+                matrix[i + studentBoxOffset, 0] = orderedStudents[i].Surname;
+            }
+
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    builder.Append(matrix[i, j] + "\t");
+                }
+                builder.AppendLine();
+            }
+
+            return StatusCode(200, builder.ToString());
         }
         #endregion
 
